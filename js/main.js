8a4cb627a11e9f28b55d69d83c1d6b95126b09df
opeCre8tivia <opegude.n3t@gmail.com>
2023-06-27 15:53:21 +0300

    dom manipulation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 const addition = (a, b) => a + b
 
 addition(2, 5)
+
+/**
+ * UNDERSTANDING JS DOM
+ */
+
+const box = document.getElementById("dom")
+console.log(box)
+box.style.background = "red"
+box.style.width = "300px"
+box.style.height = "300px"
+
+box.addEventListener("click", (e) => {
+  console.log("---- iam clicked ---")
+  box.style.background = "blue"
+})
